87319a1ecdb8a7b819cb37dbaa708377843dd316
k0michi <k0michi@koyomi.co>
2026-09-17 00:50:11 +0900

    build: set DAWN_ENABLE_INSTALL to off

--- scripts/build-dawn.sh
+++ scripts/build-dawn.sh
@@ -5,7 +5,6 @@ set -eu
 repository_root=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 dependency_directory="$repository_root/.build/dependencies/dawn"
 build_directory="$dependency_directory/cmake-build"
-install_directory="$dependency_directory/install"
 swift_bin_directory=$(dirname "$(command -v swiftc)")
 
 set -- \
@@ -14,7 +13,6 @@ set -- \
     -S "$repository_root/cmake/Dawn" \
     -B "$build_directory" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="$install_directory" \
     -DDAWN_METADATA_DIRECTORY="$dependency_directory" \
     -DCMAKE_C_COMPILER="$swift_bin_directory/clang" \
     -DCMAKE_CXX_COMPILER="$swift_bin_directory/clang++"
@@ -25,4 +23,3 @@ fi
 
 cmake "$@"
 cmake --build "$build_directory" --config Release --target webgpu_dawn --parallel
-cmake --install "$build_directory" --config Release
